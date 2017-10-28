5121808f1f9808d05a0abed48630459feb23dbb6
Lucian Leustean <lucian.leustean@gmail.com>
2017-10-29 02:14:32 +0300

    Add Cassandra Repo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule CassandraPlayground.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:cqerl, github: "matehat/cqerl", tag: "v1.0.8"},
+      {:cassandra_ecto, github: "vintikzzz/cassandra_ecto"},
     ]
   end
 
